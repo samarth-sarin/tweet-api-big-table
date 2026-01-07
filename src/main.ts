969f00f6b73acf4136c07d467260575e8775e921
Samarth <samsarin@umich.edu>
2026-01-07 11:29:11 -0500

    adding debug logs

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   console.log('DB_HOST:', process.env.DB_HOST);
   console.log('DB_NAME:', process.env.DB_NAME); 
   const app = await NestFactory.create(AppModule);
+  console.log('Starting server...');
   const port = process.env.PORT || 3000;
+  console.log(`Listening on port ${port}`);
   await app.listen(port);
 }
 bootstrap();
