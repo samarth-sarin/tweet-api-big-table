21da95a5e36b2220d2e9fff5b99e9262efb5a602
Samarth <samsarin@umich.edu>
2026-01-07 11:26:31 -0500

    changing port in main.ts to pull from env to resolve gcp deployment issue

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   console.log('DB_HOST:', process.env.DB_HOST);
   console.log('DB_NAME:', process.env.DB_NAME); 
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
