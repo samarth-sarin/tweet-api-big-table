94ba58bbcd384317cdd91c101797e53fd03a3659
Samarth <samsarin@umich.edu>
2026-01-09 15:03:39 -0500

    removing tweet write to postgres and only keeping bigtable

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Tweet } from './tweet.model';
 import { BigtableService } from '../bigtable/bigtable.service';
+import { randomUUID } from 'crypto';
 
 
 @Injectable()
@@ -12,27 +13,24 @@ export class TweetService {
     private bigtableService: BigtableService,
   ) {}
 
-  async createTweet(tweetContent: string, userId: string) {
-    const tweet = await this.tweetModel.create({
+async createTweet(tweetContent: string, userId: string) {
+    const tweetId = randomUUID();
+    const createdAt = new Date();
+
+    const tweet = {
+      tweetId,
       userId,
       tweetContent,
-    });
+      createdAt,
+    };
 
-    await this.bigtableService.writeTweet({
-        tweetId: tweet.tweetId,
-        userId: tweet.userId,
-        tweetContent: tweet.tweetContent,
-        createdAt: tweet.createdAt,
-    });
+    await this.bigtableService.writeTweet(tweet);
 
     return tweet;
-  }
+}
 
   async findByUserId(userId: string) {
-    return this.tweetModel.findAll({
-      where: { userId },
-      order: [['createdAt', 'DESC']],
-    })
+    return await this.bigtableService.readTweetsByUserId(userId);
   }
 
   async findAll(): Promise<Tweet[]> {
